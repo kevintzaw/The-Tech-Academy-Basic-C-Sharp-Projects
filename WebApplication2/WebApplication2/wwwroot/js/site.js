b22fff67a0ba386ca2e2d53dddc8b2de305f77a7
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-04-22 13:23:43 -0800

    main

---
+++ WebApplication2/WebApplication2/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var div = document.getElementById('GFG_DIV');
+
+//Submit Button Function
+function GFG_Fun() {
+    div.parentNode.removeChild(div);
+    down.innerHTML ="Thank you for your answers. An Instructor will respond to this shortly. Have a great day!"
+}
