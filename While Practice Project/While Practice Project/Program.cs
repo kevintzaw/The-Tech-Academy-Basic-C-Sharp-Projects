ed75cc2a61a16ddb02cdce51d172c338686f21ff
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-04-23 18:34:46 -0800

    changes

---
+++ While Practice Project/While Practice Project/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace While_Practice_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // While Statement
+            Console.WriteLine("How Old Are You?");
+
+            int howOldAreYou = Convert.ToInt32(Console.ReadLine());
+            while (howOldAreYou > 20)
+            {
+                Console.WriteLine(howOldAreYou);
+                howOldAreYou++;
+            }
+
+            //Do While Statement
+            Console.WriteLine("How Old is Your Pet?");
+
+            int howOldisPet = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(howOldisPet);
+                howOldisPet++;
+            }
+            while (howOldisPet < 10);
+        }
+    }
+}
