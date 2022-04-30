be4d655d81043098a370736c480ab2ca2da600c1
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-04-30 06:52:15 -0800

    main

--- Strings and Integers/Strings and Integers/Program.cs
+++ Strings and Integers/Strings and Integers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Strings_and_Integers
 {
@@ -6,7 +7,37 @@ namespace Strings_and_Integers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //List of Integers
+            List<int> myValues = new List<int>();
+            myValues.Add(1);
+            myValues.Add(3);
+            myValues.Add(4);
+            myValues.Add(22);
+            myValues.Add(35);
+
+            try
+            {
+                //User Input
+                //Asking User to input number
+                Console.Write("Pick a number: ");
+                string inputNumber = Console.ReadLine();
+                int valueNumber = Convert.ToInt32(inputNumber);
+
+                //divides each number in list
+                for (int j = 0; j < myValues.Count; j++)
+                {
+                    int dividedList = valueNumber / myValues[j];
+                    Console.WriteLine(dividedList);
+                }
+            }
+
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
