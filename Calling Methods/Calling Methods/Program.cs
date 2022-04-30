cb113797133337326a616642f3a89f8276e5c16a
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-04-30 08:25:31 -0800

    main

---
+++ Calling Methods/Calling Methods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Calling_Methods
+{
+    
+    class Program
+    {
+        //addition method
+        static void additionMethod(int userValue)
+        {
+            Console.WriteLine(userValue + 7);
+        }
+
+        //subtraction method
+        static void subtractionMethod(int userValue)
+        {
+            Console.Write(userValue - 7);
+        }
+
+        //multiply method
+        static void multiplyMethod(int userValue)
+        {
+            Console.Write(userValue * 7);
+
+        }
+
+        static void Main(string[] args)
+        {
+            //user input
+            Console.WriteLine("What number do you want to use for this operation?");
+            string userInput = Console.ReadLine();
+            int userValue = Convert.ToInt32(userInput);
+
+            //calling methods and displayed integers
+            additionMethod(userValue);
+            subtractionMethod(userValue);
+            multiplyMethod(userValue);
+
+
+
+        }
+    }
+}
---
+++ Calling Methods/Calling Methods/operationsClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class operationsClass
+{
+	
+		static void additionMethod(int userValue)
+        {
+			Console.WriteLine(userValue + 7);
+        }
+
+		static void subtractionMethod(int userValue)
+        {
+			Console.Write(userValue - 7);
+        }
+
+		static void multiplyMethod(int userValue)
+        {
+			Console.Write(userValue * 7);
+
+        }
+	
+}
