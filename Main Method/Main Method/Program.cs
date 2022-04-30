9739c22dc7d21a0c4470cbdd5890520e2381a24a
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-04-30 15:46:00 -0800

    main

---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Main_Method
+{
+
+    //New class
+    class operationClass
+    {
+        //Method for integer with addition
+        static void additionMethod()
+        {
+            int basicInteger = 7;
+            int resultInteger = basicInteger + 9;
+            Console.WriteLine(resultInteger);
+        }
+        //Method for decimal with addition
+        static void decimalAdditionMethod()
+        {
+            double a = 1.5;
+            double b = 3.6;
+            Console.WriteLine(a + b);
+        }
+        //Method for string convert to integer for addition
+        static void stringAdditionMethod()
+        {
+            string stringNumber = "7";
+            int stringInteger = Convert.ToInt32(stringNumber);
+            int resultString = (stringInteger + 7);
+            Console.WriteLine(resultString);
+        }
+
+        static void Main(string[] args)
+        {
+            additionMethod();
+
+        }
+
+    }
+
+    //Main Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
