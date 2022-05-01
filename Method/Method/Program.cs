7e1fe0e5e3826e262b0d6e2c16d6c3f1e73ab96f
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-04-30 16:15:59 -0800

    main

---
+++ Method/Method/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Method
+{
+    //class
+    public class mathOperation
+    {
+        public static void integerMethod()
+        {
+            int firstNum;
+            int secondNum;
+        }
+
+    }
+    
+    //Main Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers. The second number is optional");
+            string firstString = Console.ReadLine();
+            int firstNum = Convert.ToInt32(firstString);
+            string secondString = Console.ReadLine();
+            int secondNum = Convert.ToInt32(secondString);
+            //adding two of the numbers
+            Console.WriteLine(firstNum + secondNum);
+
+        }
+    }
+}
