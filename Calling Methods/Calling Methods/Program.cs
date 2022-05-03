4133185671176faea345a35442d83b97b4196c3e
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-05-03 11:26:21 -0800

    main

--- Calling Methods/Calling Methods/Program.cs
+++ Calling Methods/Calling Methods/Program.cs
@@ -8,15 +8,20 @@ namespace Calling_Methods
 
         static void Main(string[] args)
         {
+            //instantiate class
+            operationsClass passthroughValue = new operationsClass();
+            
             //user input
             Console.WriteLine("What number do you want to use for this operation?");
             string userInput = Console.ReadLine();
             int userValue = Convert.ToInt32(userInput);
 
             //calling methods and displayed integers
-            additionMethod(userValue);
-            subtractionMethod(userValue);
-            multiplyMethod(userValue);
+            Console.WriteLine(passthroughValue.additionMethod(userValue));
+            Console.WriteLine(passthroughValue.subtractionMethod(userValue));
+            Console.WriteLine(passthroughValue.multiplyMethod(userValue));
+
+     
 
         }
     }
