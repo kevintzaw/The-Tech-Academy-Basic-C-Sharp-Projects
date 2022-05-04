7f379e3f454e15c2d59ade3ef933067d54491409
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-05-03 17:31:11 -0800

    main

--- Method/Method/Program.cs
+++ Method/Method/Program.cs
@@ -7,12 +7,13 @@ namespace Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter two numbers. The second number is optional");
+            Console.WriteLine("Enter a number.");
             Class1 newObject = new Class1();
             string firstString = Console.ReadLine();
             int x = Convert.ToInt32(firstString);
             try
             {
+                Console.WriteLine("Enter another number. It's optional if you don't.");
                 string secondString = Console.ReadLine();
                 int y = Convert.ToInt32(secondString);
                 Console.WriteLine(newObject.IntegerMethod(x, y));
