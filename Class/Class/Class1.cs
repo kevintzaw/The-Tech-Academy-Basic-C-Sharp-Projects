760ca9c5440cbb500f1a9134c4846bcff2631924
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-05-04 19:51:53 -0800

    main

---
+++ Class/Class/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class1
+{
+    public class mathOperation
+    {
+        //dividing method
+        public void oneInteger(int userInteger)
+        {
+            //math operation on integer
+            int resultInteger = userInteger / 2;
+            //display the resulting integer on the screen
+            Console.WriteLine(resultInteger);
+        }
+
+        //overloading a method
+        public void oneInteger(int oneInteger, int twoInteger)
+        {
+            int resultInteger = oneInteger + twoInteger;
+            Console.WriteLine(resultInteger);
+        }
+    }
+}
--- Class/Class/Program.cs
+++ Class/Class/Program.cs
@@ -1,27 +1,29 @@
-﻿using System;
+﻿using Class1;
+using System;
 
 namespace Class
 {
     public class Program
-    {
-        //void method that outputs an integer
-        public static void divInteger(int initialInteger)
-        {
-            int resultInteger = initialInteger / 2;
-            Console.WriteLine(resultInteger);
-        }
+    { 
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //instantiate class
-            Program myObj = new Program();
+            //instantiating a class
+            Class1.mathOperation obj1 = new mathOperation();
 
             //ask the user for a number
             Console.WriteLine("Input a Number");
             string userValue = Console.ReadLine();
             int userNumber = Convert.ToInt32(userValue);
-            divInteger(userNumber);
+
+            //calling the method to the user number
+            obj1.mathOperation(userNumber);
+
+            //creating a method with parameters
+            int outputVariable;
+            mathOperation(outputVariable);
+            mathOperation(out int outputVariable);
+            
 
         }
     }
