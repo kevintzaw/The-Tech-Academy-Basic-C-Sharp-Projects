9d9196000cd303c583b5482f482c864afe104ebc
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-05-06 20:47:15 -0800

    main

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    abstract class Employee:IQuittable
+    {
+        public abstract void Quit();
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //trying to create instance of Interface
+            IQuittable newObj = new IQuittable();
+            //called method
+            newObj.Quit();
+
+        }
+    }
+}
