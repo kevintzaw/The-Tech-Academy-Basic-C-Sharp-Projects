1433256f43c8946c0b1489eb7d2a1f4ed4d11f99
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-05-09 19:13:57 -0800

    main

---
+++ Operatores/Operatores/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operatores
+{
+    class Employee
+    {
+        public int number1, number2;
+        public Employee(int num1, int num2)
+        {
+            number1 = num1;
+            number2 = num2;
+        }
+    }
+}
---
+++ Operatores/Operatores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operatores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newobj = new Employee(15, 25);
+
+            //overloading operators
+            _ = newobj == newobj;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
