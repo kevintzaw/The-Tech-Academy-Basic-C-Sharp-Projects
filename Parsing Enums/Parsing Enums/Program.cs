52716b62a45b69fd1fd06893448164133ffaaf13
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-05-09 19:31:59 -0800

    main

---
+++ Parsing Enums/Parsing Enums/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        //enum for days of the week
+        enum daysoftheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            //user inputs value
+            Console.WriteLine("Enter the current day of the week");
+            string current = Console.ReadLine();
+
+            try
+            {
+
+                foreach (string days in daysoftheWeek)
+                {
+                    daysoftheWeek current = daysoftheWeek.days;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("A day of the week needs to be entered");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
