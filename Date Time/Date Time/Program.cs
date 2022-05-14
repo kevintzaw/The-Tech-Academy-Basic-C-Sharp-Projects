fe21f8621b2fd42ee6f080efd7d10a26ef912c8f
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-05-13 21:19:33 -0800

    main

---
+++ Date Time/Date Time/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string for current time
+            string dateTime = DateTime.Now.ToString("hh:mm:ss tt");
+            Console.WriteLine(dateTime);
+
+            //convert current time to int
+            int newCalTime = Convert.ToInt32(dateTime);
+            //user input value
+            Console.WriteLine("Input a number value here");
+            string userInput = Console.ReadLine();
+            int userTimeInput = Convert.ToInt32(userInput);
+
+            //calculation
+            int intResultTime = userTimeInput + newCalTime;
+
+            //convert Number to Time
+            string stringResultTime = intResultTime.ToString();
+            Console.WriteLine(stringResultTime);
+        }
+    }
+}
