435d540b7f8ab0fc9c060514ee92c4d301175fca
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-05-15 19:44:18 -0800

    main

---
+++ Constructor/Constructor/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    public class Employee
+    {
+        public int employeeNumber;
+        //variable using the keyword "var"
+        public string var;
+
+        public Employee(){
+            Console.WriteLine("Reserving new record for the upcoming employee");
+        }
+        public Employee(string var){
+            this.var = var;
+            Console.WriteLine("Creating new record for the upcoming employee, with variable string");
+        }
+        public Employee( string var, int employeeNumber){
+            this.var = var;
+            this.employeeNumber = employeeNumber;
+            Console.WriteLine("Creating new record for the upcoming employee with variable string and employeeNumber.");
+        }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        //chainning constructors together
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First construct!");
+            Employee a = new Employee();
+            Console.WriteLine("Second construct!");
+            Employee b = new Employee("Steve");
+            Console.WriteLine("Third construct!");
+            Employee c = new Employee("Steve", 5);
+        }
+    }
+}
